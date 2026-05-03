5a903976f493490c96902a31ce694c75840fc10e
HWA <antony.i.sta@gmail.com>
2026-05-03 12:37:39 +0300

    Add files via upload

---
+++ VariaType/usefull_files/setup.py
@@ -0,0 +1,29 @@
+import os
+
+from fontTools.fontBuilder import FontBuilder
+from fontTools.pens.ttGlyphPen import TTGlyphPen
+
+def create_source_font(filename, weight=400):
+    fb = FontBuilder(unitsPerEm=1000, isTTF=True)
+    fb.setupGlyphOrder([".notdef"])
+    fb.setupCharacterMap({})
+    
+    pen = TTGlyphPen(None)
+    pen.moveTo((0, 0))
+    pen.lineTo((500, 0))
+    pen.lineTo((500, 500))
+    pen.lineTo((0, 500))
+    pen.closePath()
+    
+    fb.setupGlyf({".notdef": pen.glyph()})
+    fb.setupHorizontalMetrics({".notdef": (500, 0)})
+    fb.setupHorizontalHeader(ascent=800, descent=-200)
+    fb.setupOS2(usWeightClass=weight)
+    fb.setupPost()
+    fb.setupNameTable({"familyName": "Test", "styleName": f"Weight{weight}"})
+    fb.save(filename)
+
+if __name__ == '__main__':
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    create_source_font("source-light.ttf", weight=100)
+    create_source_font("source-regular.ttf", weight=400)
